280165f7fb3572c3ba9347d51f29ced27ad5c9af
Asheesh25 <asheesh.upadhyay2512@gmail.com>
2021-09-28 19:34:38 +0530

    Comparison Operator

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-b=11
-console.log('b= ',b)
-b+=10
-console.log('b+=10 ',b)
-b++
-console.log('b++ ',b)
-b-=10
-console.log('b-=10 ',b)
+var d=10
+var e=2
+var f='10'
+console.log(d!=e)
+console.warn(d==f)
+console.log(d===f)
+console.log(d+f)
