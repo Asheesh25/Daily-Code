55b9b7977445cce6e382d1dc3b069c81b3c59044
Asheesh25 <asheesh.upadhyay2512@gmail.com>
2021-10-09 21:08:22 +0530

    Primitive datatype

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-console.log("true && true=",true&&true)
-console.log("true && false=",true&&false)
-console.log("false && true=",false&&true)
-console.log("false && false=",false&&false)
-console.log("true || true=",true||true)
-console.log("true || false=",true||false)
-console.log("false || true=",false||true)
-console.log("false || false=",false||false)
+//Console object
+
+console.log("Hello World")
+console.warn("This is warning")
+console.error("This is error")
+
+//String
+console.log(typeof "Asheesh")
+console.log(typeof 'a')
+
+//Numbers
+console.log(typeof 11)
+console.log(typeof -1.11)
+
+//Boolean
+console.log(typeof true)
+console.log(false)
