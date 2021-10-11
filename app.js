e9d073e082acf9466264140fce57ddafc7117044
Asheesh25 <asheesh.upadhyay2512@gmail.com>
2021-10-11 19:38:46 +0530

    innerHTML

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 var heading = document.getElementById('heading')//This getElementById is a method of DOM API that gets us the element by it's id. 
-console.log(heading)
+var para = document.getElementsByClassName('para')
+console.log(para)
+heading.innerHTML = "I am so great"
+console.log(heading)//Heading and document are objects
