c107bc965acb2ae102c0d94d0b689484571dce0e
Asheesh25 <asheesh.upadhyay2512@gmail.com>
2021-10-12 23:55:48 +0530

    DOM methods

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var heading = document.getElementById('heading')//This getElementById is a method of DOM API that gets us the element by it's id. 
-var para = document.getElementsByClassName('para')
-console.log(para)
-heading.innerHTML = "I am so great"
-console.log(heading)//Heading and document are objects
+var heading = document.getElementById('heading') 
+var para = document.getElementById('para')
+var tags = document.getElementsByTagName('a') 
+var query = document.querySelector('.para') 
+
+console.log(query)
