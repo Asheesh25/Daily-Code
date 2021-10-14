e9e8dd2ab0d1b0820a0e07f8441e8b39496cf72f
Asheesh25 <asheesh.upadhyay2512@gmail.com>
2021-10-15 00:03:31 +0530

    todo list implementation

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var button = document.querySelector('button')
 var input = document.querySelector('input')
 var list = document.querySelector('ul') 
 
-const callback = ()=>{
-  console.log('Ashu')
-  console.log('Minu')
+const callback = (event)=>{
+    const inputValue = input.value
+    const element = document.createElement('li')
+    const textNode = document.createTextNode(inputValue)
+    element.appendChild(textNode)
+    list.appendChild(element)
 }
 //On clicking the button, the callback function is executed
-button.addEventListener('click',callback)
+button.addEventListener('click',callback) 
