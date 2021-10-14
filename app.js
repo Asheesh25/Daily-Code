a29be83d7c201b33295fd13ad9637be8499038b1
Asheesh25 <asheesh.upadhyay2512@gmail.com>
2021-10-14 10:58:47 +0530

    Default value in function

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-var list = document.querySelector('ul')
-console.log(list)
-var arr = ['Go to the Gym','Cook Oatmeal','Eat']
-arr.push('Randi')
-for(var i=0;i<arr.length;i++)
+//default value('Minu')
+function greet(name='Minu')
 {
-  var element = document.createElement('li')
-  var textNode = document.createTextNode(arr[i])
-  element.appendChild(textNode)
-  list.appendChild(element)
+  console.log("I am "+name)
 }
+greet()
